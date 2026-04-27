a45a1a48d18bf9bb2ddbb9dfe492f463451c1daf
Rafael Solis Ramos <rafaelsolioramoso@gmail.com>
2026-04-27 16:52:30 +0200

    q_a fix

---
+++ frontend/src/app/core/models/auth.interface.ts
@@ -0,0 +1,22 @@
+export interface LoginResponse {
+  token: string;
+}
+
+export interface JwtDecoded {
+  id:number;
+  role:string;
+  exp: number;
+}
+
+export interface RegisterData {
+  username:string;
+  email:string;
+  password:string;
+  birthdate:string;
+  gender:string;
+}
+
+export interface LoginData {
+  username:string;
+  password:string;
+}
