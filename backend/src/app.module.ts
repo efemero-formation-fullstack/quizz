97bcd13b85e0c1d1bcfd2684226a3bab06cceafa
François Bastien <fmrbastien@gmail.com>
2026-04-27 08:31:25 +0200

    typeorm base config

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,27 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggingMiddleware } from './middleware/logging.middleware';
 
+console.log(process.env);
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: +process.env.POSTGRES_PORT!,
+      database: process.env.POSTGRES_DB,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      synchronize: true,
+      logging: true,
+      entities: [],
+    }),
+    TypeOrmModule.forFeature([]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
