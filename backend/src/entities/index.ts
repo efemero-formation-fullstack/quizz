baf2d696d1addeff524165017ef2385de54215dc
François Bastien <fmrbastien@gmail.com>
2026-04-28 09:06:52 +0200

    re-add missing entities, services and controllers to appmodule (#35)

---
+++ backend/src/entities/index.ts
@@ -0,0 +1,5 @@
+export { AnswerEntity } from './answer.entity';
+export { QuestionEntity } from './question.entity';
+export { QuizzEntity } from './quizz.entity';
+export { ThemeEntity } from './theme.entity';
+export { UserEntity } from './user.entity';
