3707d4ed16a0d09231df6cbc6df3ae7e26dcb99e
François Bastien <fmrbastien@gmail.com>
2026-04-28 09:06:21 +0200

    re-add missing entities, services and controllers to appmodule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,11 +5,26 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { LoggingMiddleware } from './middlewares/logging.middleware';
-import { UserEntity } from './entities/user.entity';
-import { QuizzEntity } from './entities/quizz.entity';
-import { ThemeEntity } from './entities/theme.entity';
+import { AnswerController } from './controllers/answer/answer.controller';
+import { AuthController } from './controllers/auth/auth.controller';
+import { QuizzController } from './controllers/quizz/quizz.controller';
+import { ThemeController } from './controllers/theme/theme.controller';
+import { UserController } from './controllers/user/user.controller';
+import {
+  AnswerEntity,
+  QuestionEntity,
+  QuizzEntity,
+  ThemeEntity,
+  UserEntity,
+} from './entities/index';
 import { AuthMiddleware } from './middlewares/auth/auth.middleware';
+import { LoggingMiddleware } from './middlewares/logging.middleware';
+import { AnswerService } from './services/answer/answer.service';
+import { AuthService } from './services/auth/auth.service';
+import { QuestionService } from './services/question/question.service';
+import { QuizzService } from './services/quizz/quizz.service';
+import { ThemeService } from './services/theme/theme.service';
+import { UserService } from './services/user/user.service';
 
 console.log(process.env);
 @Module({
@@ -28,13 +43,41 @@ console.log(process.env);
       password: process.env.POSTGRES_PASSWORD,
       synchronize: true,
       logging: true,
-      entities: [UserEntity, QuizzEntity, ThemeEntity],
+      entities: [
+        UserEntity,
+        QuizzEntity,
+        ThemeEntity,
+        QuestionEntity,
+        AnswerEntity,
+      ],
       namingStrategy: new SnakeNamingStrategy(),
     }),
-    TypeOrmModule.forFeature([UserEntity, QuizzEntity, ThemeEntity]),
+    TypeOrmModule.forFeature([
+      UserEntity,
+      QuizzEntity,
+      ThemeEntity,
+      QuestionEntity,
+      AnswerEntity,
+    ]),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [
+    AppController,
+    AnswerController,
+    AuthController,
+    QuizzController,
+    QuizzController,
+    ThemeController,
+    UserController,
+  ],
+  providers: [
+    AppService,
+    AnswerService,
+    AuthService,
+    QuestionService,
+    QuizzService,
+    ThemeService,
+    UserService,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
